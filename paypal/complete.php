cf95751258c17a55d5dc228579f71a34c63c7a2c
lifo <lifo.java@yahoo.com>
2013-06-03 17:07:26 +0600

    paypal and slick tabs
    practice from nettuts

---
+++ paypal/complete.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+	</head>
+	
+	<body>
+		<h1>Payment Complete</h1>
+		<hr>
+		<p>Thank you for your donation to Nettuts+. We value your support and hope you are enjoying the site. Thanks again!</p>
+	</body>
+</html>
---
+++ slick_tabbed_content/index.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+		<script src="functions.js" type="text/javascript"></script>
+	</head>
+	
+	<body>
+	
+		<div id="tabbed_box_1" class="tabbed_box">  
+    <h4>Browse Site <small>Select a Tab</small></h4>  
+    <div class="tabbed_area">  
+      
+        <ul class="tabs">  
+            <li><a href="#" title="content_1" class="tab active">Topics</a></li>  
+			<li><a href="#" title="content_2" class="tab">Archives</a></li>  
+			<li><a href="#" title="content_3" class="tab">Pages</a></li>  
+        </ul>  
+          
+        <div id="content_1" class="content">  
+            <ul>  
+                <li><a href="">HTML Techniques <small>4 Posts</small></a></li>  
+                <li><a href="">CSS Styling <small>32 Posts</small></a></li>  
+                <li><a href="">Flash Tutorials <small>2 Posts</small></a></li>  
+                <li><a href="">Web Miscellanea <small>19 Posts</small></a></li>  
+                <li><a href="">Site News <small>6 Posts</small></a></li>  
+                <li><a href="">Web Development <small>8 Posts</small></a></li>  
+            </ul>  
+        </div>  
+        <div id="content_2" class="content">  
+            <ul>  
+                <li><a href="">December 2008 <small>6 Posts</small></a></li>  
+                <li><a href="">November 2008 <small>4 Posts</small></a></li>  
+                <li><a href="">October 2008 <small>22 Posts</small></a></li>  
+                <li><a href="">September 2008 <small>12 Posts</small></a></li>  
+                <li><a href="">August 2008 <small>3 Posts</small></a></li>  
+                <li><a href="">July 2008 <small>1 Posts</small></a></li>  
+            </ul>  
+        </div>  
+        <div id="content_3" class="content">  
+            <ul>  
+                <li><a href="">Home</a></li>  
+                <li><a href="">About</a></li>  
+                <li><a href="">Contribute</a></li>  
+                <li><a href="">Contact</a></li>  
+            </ul>  
+        </div>  
+      
+    </div>  
+  
+</div>  
+	
+	</body>
+</html>
